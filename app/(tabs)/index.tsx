d7d3b5810e3b53e7110417182e53034bf11996c3
Tsimmuaj Yang <tyang4204@gmail.com>
2026-02-27 05:48:56 +0000

    added flashlist of items

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,15 @@
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, StatusBar } from "react-native";
+import { FlashList } from "@shopify/flash-list";
+import {homeFeed} from "@/placeholder"
 
 export default function App() {
   return (
-    <View>
-        <Text>Homepage</Text>
+    <View style={{flex: 1, flexDirection: "column"}}>
+        <FlashList
+            data={homeFeed}
+            renderItem={({item}) => <Text>{item.image}</Text>}
+        />
     </View>
   );
 }
